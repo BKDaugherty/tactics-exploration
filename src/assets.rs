3b0300b1892f89f12c6e6b29978aad38eac6d33b
Brendon Daugherty <brendon1097@gmail.com>
2025-12-25 13:38:53 -0800

    feat(fonts): Try out custom fonts

--- src/assets.rs
+++ src/assets.rs
@@ -12,3 +12,21 @@ pub const GRADIENT_PATH: &str = "utility_assets/gradient.png";
 
 pub const EXAMPLE_MAP_PATH: &str = "map_assets/example-map/example-map.tmx";
 pub const EXAMPLE_MAP_2_PATH: &str = "map_assets/tinytactics-32-map/example-map-tiny-tactics.tmx";
+
+use bevy::prelude::*;
+
+#[derive(Resource)]
+pub struct FontResource {
+    pub fine_fantasy: Handle<Font>,
+    pub badge: Handle<Font>,
+}
+
+pub fn setup_fonts(mut commands: Commands, asset_loader: Res<AssetServer>) {
+    let badge = asset_loader.load("font_assets/tinyRPGFontKit01_v1_2/TinyRpg-BadgeFont.ttf");
+    let fine_fantasy =
+        asset_loader.load("font_assets/tinyRPGFontKit01_v1_2/TinyRpg-FineFantasyStrategies.ttf");
+    commands.insert_resource(FontResource {
+        fine_fantasy,
+        badge,
+    });
+}
